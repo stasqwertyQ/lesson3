75e2c51202253fca28e56d6a1443976a91583493
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2023-11-24 17:57:00 +0200

    alert.namber.js.js.js

---
+++ lesson3.js
@@ -0,0 +1,34 @@
+const namberOne = 5;
+const namberTwo = 5;
+const namberString = "5";
+
+console.log(namberOne + namberTwo + namberString);
+
+
+const Email = "xdtdygcgcc134@gmail.com".length;
+
+console.log(Email);
+
+
+const EmailL = "xdtdygcgcc134@gmail.com".indexOf("@");
+
+console.log(EmailL);
+
+const Name1 = " My";
+const Name2 = " name";
+const Name3 = " is";
+
+console.log(Name1 + Name2 + Name3);
+
+const FullName = " Victor";
+
+console.log(Name1 + Name2 + Name3 + FullName);
+
+
+
+const userName = "Олександр";
+
+const payment = "300";
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
